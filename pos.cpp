40d48b50614e0c56ea43cf6b04ed1fe50ca1dc32
ankitsir4u <ankitsir4u@gmail.com>
2024-12-28 10:44:39 +0530

    neg

---
+++ pos.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+
+    cin>>n;
+
+    if(n>0)
+    {
+        cout<<"positive ";
+
+    }
+else if (n==0)
+
+{
+    cout<<"0";
+}
+else
+cout<<"negative";
+}
